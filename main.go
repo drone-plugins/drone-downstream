84e2cd7d67c4a95e56d636dcf147ae4a43657bd6
Rachid Zarouali <xinity@users.noreply.github.com>
2015-12-23 15:42:45 +0100

    initial attempt to add fork option

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
 type Params struct {
 	Repos []string `json:"repositories"`
 	Token string   `json:"token"`
+	Fork string	`json:"fork"`
 }
 
 func main() {
@@ -41,14 +42,21 @@ func main() {
 			fmt.Printf("Error: unable to parse repository name %s.\n", entry)
 			os.Exit(1)
 		}
-
+		if v.fork) == "true" {
+		// start a new  build
+		_, err = client.BuildFork(owner, name, build.Number)
+			if err != nil {
+				fmt.Printf("Error: unable to trigger a new build for %s.\n", entry)
+				os.Exit(1)
+		}
+		else {
 		// get the latest build for the specified repository
 		build, err := client.BuildLast(owner, name, branch)
 		if err != nil {
 			fmt.Printf("Error: unable to get latest build for %s.\n", entry)
 			os.Exit(1)
 		}
-
+	
 		// rebuild the latest build
 		_, err = client.BuildStart(owner, name, build.Number)
 		if err != nil {
@@ -57,6 +65,7 @@ func main() {
 		}
 
 		fmt.Printf("Restarting build %d for %s\n", build.Number, entry)
+		}
 	}
 }
 
