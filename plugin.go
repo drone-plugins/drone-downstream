090f66aec1440f63a0e57c594a833d5eb1e8ced2
Rohit Gohri <gohri.rohit@gmail.com>
2019-08-01 12:06:33 +0530

    update drone client: change function calls and fields

--- plugin.go
+++ plugin.go
@@ -113,13 +113,13 @@ func (p *Plugin) Exec() error {
 					var build *drone.Build
 					if p.LastSuccessful {
 						// Get the last successful build of branch
-						builds, err := client.BuildList(owner, name)
+						builds, err := client.BuildList(owner, name, drone.ListOptions{})
 						if err != nil {
 							return fmt.Errorf("Error: unable to get build list for %s", entry)
 						}
 
 						for _, b := range builds {
-							if b.Branch == branch && b.Status == drone.StatusSuccess {
+							if b.Source == branch && b.Status == drone.StatusPassing {
 								build = b
 								break
 							}
@@ -142,7 +142,7 @@ func (p *Plugin) Exec() error {
 					}
 					if (build.Status != drone.StatusRunning && build.Status != drone.StatusPending) || !p.Wait {
 						// start a new deploy
-						_, err = client.Deploy(owner, name, build.Number, p.Deploy, params)
+						_, err = client.Promote(owner, name, int(build.Number), p.Deploy, params)
 						if err != nil {
 							if waiting {
 								continue
@@ -167,15 +167,15 @@ func (p *Plugin) Exec() error {
 					fmt.Printf("BuildLast for repository: %s, returned build number: %v with a status of %s. Will retry for %v.\n", entry, build.Number, build.Status, p.Timeout)
 					waiting = true
 					continue
-				} else if p.LastSuccessful && build.Status != drone.StatusSuccess {
-					builds, err := client.BuildList(owner, name)
+				} else if p.LastSuccessful && build.Status != drone.StatusPassing {
+					builds, err := client.BuildList(owner, name, drone.ListOptions{})
 					if err != nil {
 						return fmt.Errorf("Error: unable to get build list for %s.\n", entry)
 					}
 
 					build = nil
 					for _, b := range builds {
-						if b.Branch == branch && b.Status == drone.StatusSuccess {
+						if b.Source == branch && b.Status == drone.StatusPassing {
 							build = b
 							break
 						}
@@ -188,7 +188,7 @@ func (p *Plugin) Exec() error {
 				if (build.Status != drone.StatusRunning && build.Status != drone.StatusPending) || !p.Wait {
 					if p.Fork {
 						// start a new  build
-						_, err = client.BuildFork(owner, name, build.Number, params)
+						_, err = client.BuildRestart(owner, name, int(build.Number), params)
 						if err != nil {
 							if waiting {
 								continue
@@ -200,7 +200,7 @@ func (p *Plugin) Exec() error {
 						break I
 					} else {
 						// rebuild the latest build
-						_, err = client.BuildStart(owner, name, build.Number, params)
+						_, err = client.BuildRestart(owner, name, int(build.Number), params)
 						if err != nil {
 							if waiting {
 								continue
