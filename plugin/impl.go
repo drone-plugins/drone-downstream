e5950c9166c3cc648995ddcd192ccddaec6f2938
Don <don.j.olmstead@gmail.com>
2020-05-28 13:26:06 -0700

    Fix static checks around error strings

--- plugin/impl.go
+++ plugin/impl.go
@@ -39,7 +39,7 @@ type Settings struct {
 // Validate handles the settings validation of the plugin.
 func (p *Plugin) Validate() error {
 	if len(p.settings.Token) == 0 {
-		return fmt.Errorf("Error: you must provide your Drone access token.")
+		return fmt.Errorf("you must provide your drone access token")
 	}
 
 	p.settings.server = getServerWithDefaults(p.settings.Server, p.settings.Host, p.settings.Proto)
@@ -47,23 +47,23 @@ func (p *Plugin) Validate() error {
 		p.settings.server = getServerWithDefaults("", p.pipeline.System.Host, p.pipeline.System.Proto)
 	}
 	if len(p.settings.server) == 0 {
-		return fmt.Errorf("Error: you must provide your Drone server.")
+		return fmt.Errorf("you must provide your drone server")
 	}
 
 	if p.settings.Wait && p.settings.LastSuccessful {
-		return fmt.Errorf("Error: only one of wait and last_successful can be true; choose one")
+		return fmt.Errorf("only one of wait and last_successful can be true; choose one")
 	}
 
 	var err error
 	p.settings.params, err = parseParams(p.settings.Params.Value())
 	if err != nil {
-		return fmt.Errorf("Error: unable to parse params: %s.\n", err)
+		return fmt.Errorf("unable to parse params: %s", err)
 	}
 
 	for _, k := range p.settings.ParamsEnv.Value() {
 		v, exists := os.LookupEnv(k)
 		if !exists {
-			return fmt.Errorf("Error: param_from_env %s is not set.\n", k)
+			return fmt.Errorf("param_from_env %s is not set", k)
 		}
 
 		p.settings.params[k] = v
@@ -90,16 +90,16 @@ func (p *Plugin) Execute() error {
 		// parses the repository name in owner/name@branch format
 		owner, name, branch := parseRepoBranch(entry)
 		if len(owner) == 0 || len(name) == 0 {
-			return fmt.Errorf("Error: unable to parse repository name %s.\n", entry)
+			return fmt.Errorf("unable to parse repository name %s", entry)
 		}
 
 		// check for mandatory build no during deploy trigger
 		if len(p.settings.Deploy) != 0 {
 			if branch == "" {
-				return fmt.Errorf("Error: build no or branch must be mentioned for deploy, format repository@build/branch")
+				return fmt.Errorf("build no or branch must be mentioned for deploy, format repository@build/branch")
 			}
 			if _, err := strconv.Atoi(branch); err != nil && !p.settings.LastSuccessful {
-				return fmt.Errorf("Error: for deploy build no must be numeric only " +
+				return fmt.Errorf("for deploy build no must be numeric only " +
 					" or for branch deploy last_successful should be true," +
 					" format repository@build/branch")
 			}
@@ -119,7 +119,7 @@ func (p *Plugin) Execute() error {
 			select {
 			// Got a timeout! fail with a timeout error
 			case <-timeout:
-				return fmt.Errorf("Error: timed out waiting on a build for %s.\n", entry)
+				return fmt.Errorf("timed out waiting on a build for %s", entry)
 			// Got a tick, we should check on the build status
 			case <-tick:
 				// first handle the deploy trigger
@@ -129,7 +129,7 @@ func (p *Plugin) Execute() error {
 						// Get the last successful build of branch
 						builds, err := client.BuildList(owner, name, drone.ListOptions{})
 						if err != nil {
-							return fmt.Errorf("Error: unable to get build list for %s", entry)
+							return fmt.Errorf("unable to get build list for %s", entry)
 						}
 
 						for _, b := range builds {
@@ -139,14 +139,14 @@ func (p *Plugin) Execute() error {
 							}
 						}
 						if build == nil {
-							return fmt.Errorf("Error: unable to get last successful build for %s", entry)
+							return fmt.Errorf("unable to get last successful build for %s", entry)
 						}
 					} else {
 						// Get build by number
 						buildNumber, _ := strconv.Atoi(branch)
 						build, err = client.Build(owner, name, buildNumber)
 						if err != nil {
-							return fmt.Errorf("Error: unable to get requested build %v for deploy for %s", buildNumber, entry)
+							return fmt.Errorf("unable to get requested build %v for deploy for %s", buildNumber, entry)
 						}
 					}
 					if p.settings.Wait && !waiting && (build.Status == drone.StatusRunning || build.Status == drone.StatusPending) {
@@ -161,9 +161,9 @@ func (p *Plugin) Execute() error {
 							if waiting {
 								continue
 							}
-							return fmt.Errorf("Error: unable to trigger deploy for %s - err %v", entry, err)
+							return fmt.Errorf("unable to trigger deploy for %s - err %v", entry, err)
 						}
-						fmt.Printf("Starting deploy for %s/%s env - %s build - %d.\n", owner, name, p.settings.Deploy, build.Number)
+						fmt.Printf("starting deploy for %s/%s env - %s build - %d", owner, name, p.settings.Deploy, build.Number)
 						logParams(p.settings.params, p.settings.ParamsEnv.Value())
 						break I
 					}
@@ -175,7 +175,7 @@ func (p *Plugin) Execute() error {
 					if waiting {
 						continue
 					}
-					return fmt.Errorf("Error: unable to get latest build for %s: %s.\n", entry, err)
+					return fmt.Errorf("unable to get latest build for %s: %s", entry, err)
 				}
 				if p.settings.Wait && !waiting && (build.Status == drone.StatusRunning || build.Status == drone.StatusPending) {
 					fmt.Printf("BuildLast for repository: %s, returned build number: %v with a status of %s. Will retry for %v.\n", entry, build.Number, build.Status, p.settings.Timeout)
@@ -184,7 +184,7 @@ func (p *Plugin) Execute() error {
 				} else if p.settings.LastSuccessful && build.Status != drone.StatusPassing {
 					builds, err := client.BuildList(owner, name, drone.ListOptions{})
 					if err != nil {
-						return fmt.Errorf("Error: unable to get build list for %s.\n", entry)
+						return fmt.Errorf("unable to get build list for %s", entry)
 					}
 
 					build = nil
@@ -195,7 +195,7 @@ func (p *Plugin) Execute() error {
 						}
 					}
 					if build == nil {
-						return fmt.Errorf("Error: unable to get last successful build for %s.\n", entry)
+						return fmt.Errorf("unable to get last successful build for %s", entry)
 					}
 				}
 
@@ -206,7 +206,7 @@ func (p *Plugin) Execute() error {
 						if waiting {
 							continue
 						}
-						return fmt.Errorf("Error: unable to trigger build for %s.\n", entry)
+						return fmt.Errorf("unable to trigger build for %s", entry)
 					}
 					fmt.Printf("Restarting build %d for %s\n", build.Number, entry)
 					logParams(p.settings.params, p.settings.ParamsEnv.Value())
