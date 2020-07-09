3c9a7d276c6aecc2e1540b42e91833713aadcb2c
Dominik Schmidt <dev@dominik-schmidt.de>
2020-07-09 10:50:46 +0200

    Do not claim downstream builds can be canceled if the upstream build is canceled

--- cmd/drone-downstream/config.go
+++ cmd/drone-downstream/config.go
@@ -72,14 +72,14 @@ func settingsFlags(settings *plugin.Settings) []cli.Flag {
 		},
 		&cli.BoolFlag{
 			Name:        "block",
-			Usage:       "Block until the triggered build finished, makes this build fail if triggered build fails",
+			Usage:       "Block until the triggered build is finished, makes this build fail if triggered build fails",
 			EnvVars:     []string{"PLUGIN_BLOCK"},
 			Destination: &settings.Block,
 		},
 		&cli.DurationFlag{
 			Name:        "blockTimeout",
 			Value:       time.Duration(60) * time.Minute,
-			Usage:       "How long to block until the triggered build finished",
+			Usage:       "How long to block until the triggered build is finished",
 			EnvVars:     []string{"PLUGIN_BLOCK_TIMEOUT"},
 			Destination: &settings.BlockTimeout,
 		},
