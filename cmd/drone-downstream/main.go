7ca17b130ed7581508a07171c3354cb33c7f3705
Arve Knudsen <arve.knudsen@gmail.com>
2020-09-29 08:35:20 +0200

    Remove unnecessary change
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- cmd/drone-downstream/main.go
+++ cmd/drone-downstream/main.go
@@ -23,9 +23,7 @@ func main() {
 	settings := &plugin.Settings{}
 
 	if _, err := os.Stat("/run/drone/env"); err == nil {
-		if err := godotenv.Overload("/run/drone/env"); err != nil {
-			errors.HandleExit(err)
-		}
+		godotenv.Overload("/run/drone/env")
 	}
 
 	app := &cli.App{
